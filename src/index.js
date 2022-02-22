7ecca02db6c079f41f38764b768295a5ed1695b8
Sektor <g.sektorian@gmail.com>
2022-02-23 00:28:38 +0300

    docs: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  const arr1 = String(n).split('');
+  let arr2 = [];
+  let result;
+  for (let i = 0; i < arr1.length; i++) {
+    arr2.unshift(arr1[i]);
+  }
+  result = parseInt(arr2.join(''));
+  return result;
 }
